7008c43a729d3c088a771e92917bc312a8e1171a
Long <nguyenlong14052004@gmail.com>
2026-07-17 16:43:37 +0700

    Fix run service db first in docker compose

--- BE/db/data-source.ts
+++ BE/db/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST ?? 'localhost',
-  port:  Number(process.env.DB_PORT ?? 5432),
+  port: Number(process.env.DB_PORT ?? 5432),
   username: process.env.DB_USERNAME ?? 'postgres',
   password: process.env.DB_PASSWORD ?? '123456',
   database: process.env.DB_NAME ?? 'TasteBite',
